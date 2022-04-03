8b20494503a8b3d62be674fd5b3dd9bb7ce3bfbe
vitaly <vitaly.zubentsov@ya.ru>
2022-04-03 16:38:28 +0300

    Feature: Add picture to message

--- chirp-chirp/src/main/java/com/zubentsov/chirpchirp/domen/Message.java
+++ chirp-chirp/src/main/java/com/zubentsov/chirpchirp/domen/Message.java
@@ -11,63 +11,72 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Message {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
-	private String text;
+    private String text;
 
-	private String tag;
+    private String tag;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	// use for rename autor_id to user_id in DB
-	@JoinColumn(name = "user_id")
-	private User author;
+    @ManyToOne(fetch = FetchType.EAGER)
+    // use for rename autor_id to user_id in DB
+    @JoinColumn(name = "user_id")
+    private User author;
 
-	public Message() {
-	}
+    private String filename;
 
-	public Message(String text, String tag, User user) {
-		this.text = text;
-		this.tag = tag;
-		this.author = user;
-	}
+    public Message() {
+    }
 
-	public String getAuthorName() {
+    public Message(String text, String tag, User user) {
+        this.text = text;
+        this.tag = tag;
+        this.author = user;
+    }
 
-		return (author != null) ? author.getUsername() : "<none>";
-	}
+    public String getAuthorName() {
+        return (author != null) ? author.getUsername() : "<none>";
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getText() {
-		return text;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public void setText(String text) {
+        this.text = text;
+    }
 
-	public String getTag() {
-		return tag;
-	}
+    public String getTag() {
+        return tag;
+    }
 
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 
-	public User getAuthor() {
-		return author;
-	}
+    public User getAuthor() {
+        return author;
+    }
 
-	public void setAuthor(User author) {
-		this.author = author;
-	}
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 
 }
